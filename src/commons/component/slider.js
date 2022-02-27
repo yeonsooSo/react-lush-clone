650d6bd28da6520c3039e7285cf285fe2ab1757c
yeonsooSo <janu10.sys@gmail.com>
2022-02-27 11:11:46 +0900

    slider

--- src/commons/component/slider.js
+++ src/commons/component/slider.js
@@ -1,11 +1,33 @@
 import "../../assets/slider.css";
-import React from "react";
+import React, { useState } from "react";
 import DataSlider from "../data/data_slider";
 
 function Slider(){
+    const [slideIdx, setSlideIdx] = useState(1);
+
+    const moveDot = idx => {
+        setSlideIdx(idx);
+    }
+
+
     return (
-        <div>
-            
+        <div className="slider">
+            { DataSlider.map((obj, idx) => {
+                return (
+                    <div key={obj.id} className={slideIdx === idx + 1 ? 'active-animation' : 'slide' }>
+                        <img src={process.env.PUBLIC_URL + `/Imgs/img${idx + 1}.jpg`} />
+                    </div>
+                )
+            })}
+        <div className="dots">
+            {Array.from({ length: 4}).map((item, idx)=>(
+                <div onClick = {() => {
+                    moveDot(idx + 1);
+                }}
+                    className={slideIdx === idx + 1 ? "dot active" : "dot"}>
+                </div>
+            ))}
+        </div>
         </div>
     );
 }
